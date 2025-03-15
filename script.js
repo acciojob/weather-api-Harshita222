c238630bb8e1fb4308e06495d83ef3581455a3cd
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-15 14:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.querySelector("button")
+btn.addEventListener("click",function(){
+ fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=678813de961756d6eb594f200e842b6f
+')	
+	.then(response=>response.json())
+	.then(data=>{
+	 const weatherDiv = document.getElementById("weatherData");
+		weatherDiv.innerHTML = `Current weather in London:${data.weather[0].main}`
+	});
+}
